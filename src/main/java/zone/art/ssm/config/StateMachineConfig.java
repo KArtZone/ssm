4eb68b3c7aef6195acd7b9de01363d01efb1f927
Art Kart <kartushinav@vtb.ru>
2022-05-21 16:46:25 +0300

    Добавлена конфигурация проекта

--- src/main/java/zone/art/ssm/config/StateMachineConfig.java
+++ src/main/java/zone/art/ssm/config/StateMachineConfig.java
@@ -1,11 +1,17 @@
 package zone.art.ssm.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.AuthorizeAction;
+import org.springframework.statemachine.action.PreAuthorizeAction;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
 import zone.art.ssm.domain.PaymentEvent;
 import zone.art.ssm.domain.PaymentState;
 
@@ -21,6 +27,7 @@ import static zone.art.ssm.domain.PaymentState.*;
 @Configuration
 @Slf4j
 @EnableStateMachineFactory
+@RequiredArgsConstructor
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
 
     @Override
@@ -36,11 +43,51 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
     @Override
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions
-                .withExternal().source(NEW).target(NEW).event(PRE_AUTHORIZE)
+                .withExternal()
+                .source(NEW)
+                .target(NEW)
+                .event(PRE_AUTHORIZE)
+                .action(new PreAuthorizeAction())
                 .and()
-                .withExternal().source(NEW).target(PRE_AUTH).event(PRE_AUTH_APPROVED)
+                .withExternal()
+                .source(NEW)
+                .target(PRE_AUTH)
+                .event(PRE_AUTH_APPROVED)
                 .and()
-                .withExternal().source(NEW).target(PRE_AUTH_ERROR).event(PRE_AUTH_DECLINED);
+                .withExternal()
+                .source(NEW)
+                .target(PRE_AUTH_ERROR)
+                .event(PRE_AUTH_DECLINED)
+                .and()
+                .withExternal()
+                .source(PRE_AUTH)
+                .target(PRE_AUTH)
+                .event(AUTHORIZE)
+                .action(new AuthorizeAction())
+                .and()
+                .withExternal()
+                .source(PRE_AUTH)
+                .target(AUTH)
+                .event(AUTH_APPROVED)
+                .and()
+                .withExternal()
+                .source(PRE_AUTH)
+                .target(AUTH_ERROR)
+                .event(AUTH_DECLINED);
     }
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
+        final StateMachineListenerAdapter<PaymentState, PaymentEvent> adapter = new StateMachineListenerAdapter<>() {
+            @Override
+            public void stateChanged(State<PaymentState, PaymentEvent> from, State<PaymentState, PaymentEvent> to) {
+                log.info("SM state changed from {} to {}", from, to);
+            }
+        };
+        config.withConfiguration()
+                .autoStartup(true)
+                .listener(adapter);
+    }
+
 }
 
---
+++ src/main/java/zone/art/ssm/utils/StateMachineUtils.java
@@ -0,0 +1,16 @@
+package zone.art.ssm.utils;
+
+import org.springframework.statemachine.StateMachine;
+import zone.art.ssm.domain.PaymentEvent;
+import zone.art.ssm.domain.PaymentState;
+
+/**
+ * @author Art Kart
+ * @since 21.05.2022
+ */
+public class StateMachineUtils {
+
+    public static PaymentState getId(StateMachine<PaymentState, PaymentEvent> sm) {
+        return sm.getState().getId();
+    }
+}
